a60ce078487d74785d15a3d9fd1af3f66d2ff854
Gelson Rodrigues <GelsonMarianoRodrigues@gmail.com>
2022-05-08 19:28:25 -0300

    Added page wrapper

---
+++ components/Page.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Page = styled.div`
+  margin-top: 40px;
+  display: flex;
+  justify-content: center;
+
+  > *:not(:first-child) {
+    margin-left: 210px;
+  }
+`
+
+export default Page
