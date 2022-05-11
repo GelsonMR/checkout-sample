a6a0bea8ca450246e4008f32b1e4bdaa4d3a4476
Gelson Rodrigues <GelsonMarianoRodrigues@gmail.com>
2022-05-11 15:39:08 -0300

    Updated Page to get responsive

--- components/Page.js
+++ components/Page.js
@@ -1,12 +1,19 @@
 import styled from 'styled-components'
+import { devices } from '../utils'
 
 const Page = styled.div`
-  margin-top: 40px;
+  margin-top: 20px;
   display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
   justify-content: center;
+  padding: 0 20px 20px;
 
-  > *:not(:first-child) {
-    margin-left: 210px;
+
+  @media ${devices.mediaQueries.tablet} {
+    flex-direction: row;
+    align-items: flex-start;
+    margin-top: 40px;
   }
 `
 
