32994f7785560237e45473cda39d5c669e7818c0
Gelson Rodrigues <GelsonMarianoRodrigues@gmail.com>
2022-05-11 15:27:25 -0300

    Added text button variation

--- components/Button.js
+++ components/Button.js
@@ -1,18 +1,22 @@
 import styled from 'styled-components'
+import * as Typography from './Typography'
 
-const Button = styled.button`
+const Button = styled(Typography.Body2).attrs({ as: 'button' })`
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 25px;
   height: 50px;
-  color: #FFF;
-  background-color: ${props => props.theme.colors.primary};
+  color: ${props => props.text ? props.theme.colors.primary : '#FFF'};
+  background-color: ${props => props.text ? 'transparent' : props.theme.colors.primary};
+  font-weight: 700;
   cursor: pointer;
 
   &:disabled {
     background-color: ${props => props.theme.colors.gray4};
   }
+
 `
+
 
 export default Button
