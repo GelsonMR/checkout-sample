ffc3bfe2131ebbc624b63705bbce2e907efa9527
Gelson Rodrigues <GelsonMarianoRodrigues@gmail.com>
2022-05-16 11:17:22 -0300

    Removed e2e tests check from jest

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,7 @@ const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   resolver: '<rootDir>/jest.resolver.js',
   coverageReporters: ['json-summary', 'text', 'lcov'],
+  modulePathIgnorePatterns: ['<rootDir>/e2e/'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
